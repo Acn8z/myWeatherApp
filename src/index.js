de25afa8388224022624e3b3212196e6638846d7
Acn8z <99932896+Acn8z@users.noreply.github.com>
2022-05-08 18:30:02 -0400

    update the time

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 let day = days[now.getDay()];
 let hours = now.getHours();
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let h3 = document.querySelector("h3");
 h3.innerHTML = `${day} ${date} ${hours}:${minutes}`;
 
